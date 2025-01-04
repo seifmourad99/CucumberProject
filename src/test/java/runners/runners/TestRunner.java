c22041e2fd0e4ce8529bfd7b07aa590b69e5f8ca
seif mohamed <seif1999mo@gmail.com>
2025-01-03 19:16:30 -0800

    update in test runner

--- src/test/java/runners/runners/TestRunner.java
+++ src/test/java/runners/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = "src/main/resources/feature/Signupp.feature" , glue ="steps" , tags = "@Sanity" , plugin = {"pretty" , "html:target/reports/report.html" , })
+@CucumberOptions(features = "src/main/resources/feature" , glue ="steps" , tags = "@Sanity" , plugin = {"pretty" , "html:target/reports/report.html" , })
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
